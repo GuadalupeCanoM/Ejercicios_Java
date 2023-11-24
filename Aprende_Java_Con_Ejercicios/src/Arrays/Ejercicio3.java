a55ab203bf2956a950c48c804cb88a2d49e990c0
GuadalupeCanoM <132688456+GuadalupeCanoM@users.noreply.github.com>
2023-11-24 19:28:56 +0100

    commit

---
+++ Aprende_Java_Con_Ejercicios/src/Arrays/Ejercicio3.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+/**
+* 7.1 Arrays
+*
+* 3. Escribe un programa que lea 10 números por teclado y que luego
+* los muestre en orden inverso, es decir, el primero que se
+* introduce es el último en mostrarse y viceversa.
+*
+* @author Guadalupe Cano
+*/
+public class Ejercicio3 {
+
+    
+         public static void main(String[] args) {
+
+      
+    }
+
+}
