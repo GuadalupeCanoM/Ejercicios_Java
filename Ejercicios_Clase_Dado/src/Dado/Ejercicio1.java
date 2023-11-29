705cfcf45a802f97ef2372b2781aeb14ee2bc0e4
GuadalupeCanoM <132688456+GuadalupeCanoM@users.noreply.github.com>
2023-11-29 19:28:38 +0100

    commit

---
+++ Ejercicios_Clase_Dado/src/Dado/Ejercicio1.java
@@ -0,0 +1,22 @@
+package Dado;
+
+/**
+ * Ejercicio 1: Ojos de serpiente
+ * 
+ * Necesitas: 2 dados de 6 caras y un jugador
+ * 
+ * Cómo jugar: El objetivo del juego es sacar en los dos dados el valor 1 a la vez (los
+ * ojos de serpiente). El juego finaliza cuando se consigue esta tirada y se debe indicar
+ * el número de intentos realizados
+ * 
+ * @author Guadalupe Cano Moyano
+ */
+public class Ejercicio1 {
+
+    
+         public static void main(String[] args) {
+
+      
+    }
+
+}
