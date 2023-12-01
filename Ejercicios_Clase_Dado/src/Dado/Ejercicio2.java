e274f27fcd9c911498f09b5f9ff2f975f6362291
GuadalupeCanoM <132688456+GuadalupeCanoM@users.noreply.github.com>
2023-12-02 00:58:42 +0100

    commit

---
+++ Ejercicios_Clase_Dado/src/Dado/Ejercicio2.java
@@ -0,0 +1,25 @@
+package Dado;
+
+/**
+ * Ejercicio 2: Tres o mas
+ * 
+ * Necesitas: 5 dados de 6 caras y al menos dos jugadores
+ * 
+ * Cómo jugar: El objetivo del juego es sacar tres dados iguales o más en una tirada. 3 dados iguales puntúan con 3
+ * puntos 4 dados iguales con 6 puntos y 5 dados iguales con 12 puntos. Si nos salen dos dados iguales no puntuamos
+ * pero ganamos el derecho de volver a tirar.
+ * 
+ * Quién gana: Se establecen 10 rondas y se van anotando las puntuaciones. Quien más puntos tenga al final gana.
+ * 
+ * 
+ * @author Guadalupe Cano Moyano
+ */
+public class Ejercicio2 {
+
+    
+         public static void main(String[] args) {
+
+      
+    }
+
+}
